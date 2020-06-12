7b3079c842e63d9fbfd8630afa68ca14ee05b87d
Nitesh Mistry <9368802+NiteshMistry@users.noreply.github.com>
2020-06-12 17:15:07 -0400

    Create 06-trees-iv.py

---
+++ 02-trees/06-trees-iv.py
@@ -0,0 +1,27 @@
+# Define your "TreeNode" Python class below
+class TreeNode:
+  def __init__(self, value):
+    self.value = value
+    self.children = []
+
+  def add_child(self, child_node):
+    print("Adding " + child_node.value)
+    self.children.append(child_node)
+    
+  def remove_child(self, child_node):
+    print("Removing " + child_node.value + " from " + self.value)
+    self.children = [child for child in self.children 
+                     if child is not child_node]
+
+  def traverse(self):
+    print(self.value)
+    for child in self.children: 
+      print(child.value)
+
+root = TreeNode("CEO")
+first_child = TreeNode("Vice-President")
+second_child = TreeNode("Head of Marketing")
+
+root.add_child(first_child)
+root.add_child(second_child)
+root.traverse()
