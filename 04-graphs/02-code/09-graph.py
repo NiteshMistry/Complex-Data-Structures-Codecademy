de52759f599de338f0fd9fd85316da2bdc11187d
Nitesh Mistry <9368802+NiteshMistry@users.noreply.github.com>
2020-06-18 22:10:02 -0400

    added files

---
+++ 04-graphs/02-code/09-graph.py
@@ -0,0 +1,26 @@
+class Graph:
+  def __init__(self, directed = False):
+    self.graph_dict = {}
+    self.directed = directed
+
+  def add_vertex(self, vertex):
+    self.graph_dict[vertex.value] = vertex
+
+  def add_edge(self, from_vertex, to_vertex, weight = 0):
+    self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
+    if not self.directed:
+      self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+
+  def find_path(self, start_vertex, end_vertex):
+    start = [start_vertex]
+    while len(start) > 0:
+      current_vertex = start.pop(0)
+      print("Visiting " + current_vertex)
+      #START CODE HERE
+      if current_vertex == end_vertex:
+        return True
+      vertex = self.graph_dict[current_vertex]
+      next_vertices = vertex.get_edges()
+      start += next_vertices
+    return False
+   
